33c84a246b39c06bbd89393f2b9d53f0ec488472
Musa Berat Bahadir <musaberatbahadir@gmail.com>
2017-07-20 14:09:42 +0300

    phpunit depend. is upgraded, added a post creator to model factory, init test

---
+++ tests/Unit/Controllers/PostsControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PostsControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+    use WithoutMiddleware;
+}
